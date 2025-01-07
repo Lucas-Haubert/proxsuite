d72cd5ad2747a41fd7a9981d2411aeb49e52ae42
Lucas-Haubert <lucas.haubert@ens-paris-saclay.fr>
2025-01-07 15:13:52 +0100

    structure pre osqp

---
+++ include/proxsuite/solvers/proxqp/dense/dense.hpp
@@ -0,0 +1,10 @@
+//
+// Copyright (c) 2022 INRIA
+//
+/** \file */
+#ifndef PROXSUITE_SOLVERS_PROXQP_DENSE_DENSE_HPP
+#define PROXSUITE_SOLVERS_PROXQP_DENSE_DENSE_HPP
+
+#include "proxsuite/solvers/proxqp/dense/wrapper.hpp" // includes everything
+
+#endif /* end of include guard PROXSUITE_SOLVERS_PROXQP_DENSE_DENSE_HPP */
